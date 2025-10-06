db5bc3a056be42ea6134dfe62bc66310f5e95171
rgarcia <jrcg070495@gmail.com>
2025-10-06 16:39:51 -0600

    Creacion 2 vistas, menu y contacto

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('menu', function() {
+    return view('menu');
+});
+
+Route::get('contacto', function() {
+    return view('contacto');
+});
